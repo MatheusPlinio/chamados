4bd936e8da151f399f733be6cc0f9672722be66f
Matheus Plinio <mplinio13@gmail.com>
2022-06-30 21:55:48 -0300

    Commit - 4

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    /** @test */
+    public function check_if_user_columns_is_correct()
+    {
+        $user = new User;
+
+        $expected = 
+        [
+            'name',
+            'email',
+            'password'
+        ];
+
+        $arrayCompared = array_diff($expected, $user->getFillable());
+
+        dd($arrayCompared);
+
+        $this->assertEquals(0, count($arrayCompared));
+    }
+}
